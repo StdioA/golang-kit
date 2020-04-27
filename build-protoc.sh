d115f05a48e4582a2c5bd071cee402938682f9d5
David Dai <stdioa@163.com>
2020-04-27 15:46:47 +0800

    Download protoc from github instead of building it from source

--- build-protoc.sh
+++ build-protoc.sh
@@ -5,14 +5,11 @@ set -e
 # https://github.com/protocolbuffers/protobuf/blob/master/src/README.md
 apt-get update
 apt-get install -y autoconf automake libtool curl make g++ unzip
-git clone https://github.com/google/protobuf -b 3.6.x --depth 1
-cd ./protobuf
-git submodule update --init --recursive
-./autogen.sh
-./configure
-make
-make check
-make install
-ldconfig
+
+VERSION=3.9.0
+PROTOC_ZIP=protoc-$VERSION-linux-x86_64.zip
+curl -OL https://github.com/google/protobuf/releases/download/v$VERSION/$PROTOC_ZIP
+unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+unzip -o $PROTOC_ZIP -d /usr/local include/*
+rm -f $PROTOC_ZIP
 protoc --version
-cd ../ && rm -rf protobuf
