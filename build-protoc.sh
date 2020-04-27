a93c81a313102b37b68c31ac0e2ee2724653e387
David Dai <stdioa@163.com>
2020-04-27 15:50:23 +0800

    Upgrade protoc version

--- build-protoc.sh
+++ build-protoc.sh
@@ -6,7 +6,7 @@ set -e
 apt-get update
 apt-get install -y autoconf automake libtool curl make g++ unzip
 
-VERSION=3.9.0
+VERSION=3.11.4
 PROTOC_ZIP=protoc-$VERSION-linux-x86_64.zip
 curl -OL https://github.com/google/protobuf/releases/download/v$VERSION/$PROTOC_ZIP
 unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
